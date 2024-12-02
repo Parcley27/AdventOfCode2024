6d539fe38e62fb910fa9b3c1642bceb2f849739f
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-12-01 23:38:20 -0800

    Day 1

---
+++ Day 1-1.py
@@ -0,0 +1,29 @@
+inputFile = "Day 1 Input.txt"
+
+input = []
+
+with open(inputFile, 'r') as file:
+    for line in file:
+        input.extend(map(int, line.split()))
+
+print(input)
+
+locationsA = []
+locationsB = []
+
+for number in range(len(input)):
+    if number % 2 == 0:
+        locationsA.append(input[number])
+    
+    else:
+        locationsB.append(input[number])
+
+locationsA.sort()
+
+totalDistance = 0
+
+for pair in range(len(locationsA)):
+    distance = locationsA[pair] - locationsB[pair]
+    totalDistance += abs(distance)
+
+print(totalDistance)
---
+++ Day 1-2.py
@@ -0,0 +1,34 @@
+inputFile = "Day 1 Input.txt"
+
+input = []
+
+with open(inputFile, 'r') as file:
+    for line in file:
+        input.extend(map(int, line.split()))
+
+print(input)
+
+locationsA = []
+locationsB = []
+
+for number in range(len(input)):
+    if number % 2 == 0:
+        locationsA.append(input[number])
+    
+    else:
+        locationsB.append(input[number])
+
+locationsA.sort()
+locationsB.sort()
+
+similarityScore = 0
+
+for locationA in locationsA:
+    locationCount = 0
+
+    for locationB in locationsB:
+        if locationA == locationB: locationCount += 1
+    
+    similarityScore += locationA * locationCount
+
+print(similarityScore)
