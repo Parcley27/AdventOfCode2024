6a393c20778a87291385819c7661467e97d16764
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-12-05 22:03:32 -0800

    Day 4-1

---
+++ Day 4/Day 4-1.py
@@ -0,0 +1,63 @@
+import os
+
+inputFile = "Day 4 Input.txt"
+
+scriptDir = os.path.dirname(__file__)
+inputFile = os.path.join(scriptDir, inputFile)
+
+input = []
+
+with open(inputFile, "r") as file:
+    for line in file:
+        row = []
+        for character in line:
+            if character != "\n":
+                row.append(character)
+
+        input.append(row)
+
+directions = [
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1),
+    (-1, -1),
+    (-1, 0),
+    (-1, 1)
+
+]
+
+wordToFind = "XMAS"
+wordLetters = []
+
+for letter in wordToFind:
+    wordLetters.append(letter)
+
+occurences = 0
+
+
+for row in range(len(input)):
+    for square in range(len(input[row])):
+        for direction in directions:
+            directionSolved = True
+
+            for letterIndex in range(len(wordLetters)):
+                horizontalChange = direction[0]
+                verticalChange = direction[1]
+
+                horizontal = square + (letterIndex * horizontalChange)
+                vertical = row + (letterIndex * verticalChange)
+
+                if (vertical < 0 or vertical >= len(input)) or (horizontal < 0 or horizontal >= len(input[row])):
+                    directionSolved = False
+                    break
+
+                elif input[vertical][horizontal] != wordLetters[letterIndex]:
+                    directionSolved = False
+                    break
+            
+            if directionSolved:
+                occurences += 1
+
+print(occurences)
